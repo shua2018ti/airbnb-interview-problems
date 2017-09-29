c0ccb12b04f62502813babad01626e1162431e7a
Han Yin <naco.siren.2012@gmail.com>
2017-09-29 10:58:12 -0500

    Added House Robber II.

--- src/house_robber/Solution.java
+++ src/house_robber/Solution.java
@@ -8,6 +8,8 @@ public class Solution {
     public static void main(String[] args) {
         int r1 = rob(new int[]{1, 4, 2, 8, 5, 7});
 
+        int r2 = rob2(new int[]{4, 2, 8, 5, 7});
+
         return;
     }
 
@@ -15,13 +17,34 @@ public class Solution {
         if (nums == null || nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
 
-        int prev2 = 0, prev1 = 0;
+        int prev = 0, last = 0;
         for (int i = 0; i < nums.length; i++) {
-            int cur = Math.max(prev2 + nums[i], prev1);
+            int cur = Math.max(prev + nums[i], last);
 
-            prev2 = prev1;
-            prev1 = cur;
+            prev = last;
+            last = cur;
         }
-        return prev1;
+        return last;
+    }
+
+    public static int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        /* Rob the first house */
+        int prev1 = nums[0], last1 = prev1;
+
+        /* Do not rob the first house */
+        int prev2 = 0, last2 = nums[1];
+
+        for (int i = 2; i < nums.length; i++) {
+            int cur1 = Math.max(prev1 + nums[i], last1);
+            prev1 = last1; last1 = cur1;
+
+            int cur2 = Math.max(prev2 + nums[i], last2);
+            prev2 = last2; last2 = cur2;
+        }
+
+        return Math.max(prev1, last2); // We've robbed the first house, therefore "last1" can not be robbed.
     }
 }
